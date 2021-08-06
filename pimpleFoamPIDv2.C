d6a8b929b06cbfa28ed666ca038d038224112dbe
FlavioMartins <flavio.martins@outlook.com>
2021-08-06 10:59:20 -0400

    Uploaded the files

---
+++ pimpleFoamPIDv2.C
@@ -0,0 +1,156 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011-2016 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    pimpleFoam
+
+Description
+    Large time-step transient solver for incompressible, turbulent flow, using
+    the PIMPLE (merged PISO-SIMPLE) algorithm.
+
+    Sub-models include:
+    - turbulence modelling, i.e. laminar, RAS or LES
+    - run-time selectable MRF and finite volume options, e.g. explicit porosity
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "dynamicFvMesh.H"
+#include "singlePhaseTransportModel.H"
+#include "kinematicMomentumTransportModel.H"
+#include "pimpleControl.H"
+#include "CorrectPhi.T.H"
+#include "fvOptions.H"
+#include "localEulerDdtScheme.H"
+#include "fvcSmooth.H"
+#include <vector>
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    #include "postProcess.H"
+
+    #include "setRootCase.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+    #include "createControl.H"
+    #include "createTimeControls.H"
+    #include "createFields.H"
+	  #include "initializeEp.H" // (v1.0)
+    #include "createFvOptions.H"
+    #include "initContinuityErrs.H"
+
+    turbulence->validate();
+
+    if (!LTS) // (v2.0)
+    {
+      #include "CourantNo.H"
+      #include "setInitialDeltaT.H"
+    }
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info<< "\nStarting time loop\n" << endl;
+
+    // (v2.0) Lines 78-94
+    while (pimple.run(runTime))
+    {
+        #include "readDyMControls.H"
+
+        if (LTS)
+        {
+            #include "setRDeltaT.H"
+        }
+        else
+        {
+            #include "CourantNo.H"
+            #include "setDeltaT.H"
+        }
+
+        runTime++;
+
+        Info<< "Time = " << runTime.timeName() << nl << endl;
+
+        #include "resetEp.H" // the PIC controller is reseted at every updated time-step
+
+        // --- Pressure-velocity PIMPLE corrector loop
+        while (pimple.loop())
+        {
+          if (pimple.firstPimpleIter() || moveMeshOuterCorrectors)
+          {
+              mesh.update();
+
+              if (mesh.changing())
+              {
+                  MRF.update();
+
+                  if (correctPhi)
+                  {
+                      // Calculate absolute flux
+                      // from the mapped surface velocity
+                      phi = mesh.Sf() & Uf();
+
+                      #include "correctPhi.H"
+
+                      // Make the flux relative to the mesh motion
+                      fvc::makeRelative(phi, U);
+                  }
+
+                  if (checkMeshCourantNo)
+                  {
+                      #include "meshCourantNo.H"
+                  }
+              }
+          }
+
+          #include "UEqn.H"
+
+          // --- Pressure corrector loop
+          while (pimple.correct())
+          {
+              #include "calculateEp.H" // (v2.0) calculate pressure error inside pressure corrector loop
+              #include "pEqn.H"
+          }
+
+          if (pimple.turbCorr())
+          {
+              laminarTransport.correct();
+              turbulence->correct();
+          }
+        }
+
+        runTime.write();
+
+        Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+            << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+            << nl << endl;
+    }
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
